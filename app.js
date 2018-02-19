b661ff2d672d261a595cffeecc9a60da24db18ec
Jeff Reiner <theundeadghost@gmail.com>
2018-02-20 00:22:27 +0100

    :bug: Fixing spike from ignoring _redirects file

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const env = process.env.SPIKE_ENV
 
 module.exports = {
   devtool: 'source-map',
-  ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
+  ignore: ['**/layout.html', 'assets/**/_*', 'views/**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json', 'netlify.toml'],
+  dumpDirs: ['views', 'assets', 'netlify'],
   reshape: htmlStandards({
     locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
     minify: env === 'production'
